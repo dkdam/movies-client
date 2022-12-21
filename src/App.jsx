66a826c1568c4bbfb6dbedef8fd23f09d47c377f
dkdam <dkdam@live.com>
2022-12-21 23:06:37 +1100

    setting up axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
+import Movies from './components/Movies'
+
+const URL = "http://http://localhost:3000/api/v1/movies"
+
+const getApiData = () => {
+  return axios.get(URL).then((res) => res.data)
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    let mounted = true
+    getApiData().then((items) => {
+      if (mounted) {
+        setMovies(items)
+      }
+    })
+    return () => (mounted = false)
+  },[])
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello You</h1>
+      <Movies movies = {movies} />
     </div>
   )
 }
